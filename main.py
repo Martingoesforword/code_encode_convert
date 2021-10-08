33c534d343323a3fee2aa822c19901b82528bff0
jiaheng.song <jiaheng.song@whaty.com>
2021-10-08 17:42:14 +0800

    支持多次转化，只真正转化一次；优化代码注释；

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import struct
 FileType, FileList = [], []
 CFG_SET_BOM = True
 CFG_SET_BOM_TYPE = "utf-8"
+# 目标编码格式
+TargetCoding = "utf-8"
+# 包括文件类型扩展名
+FileType = [".c", ".h", ".cpp", ".hpp", ".cc"]
 
 
 def get_file_list(Dir):
@@ -20,66 +24,74 @@ def get_file_list(Dir):
     FileList.extend(fileList)
     # 递归字文件夹
     for subfolder in folderList:
-            get_file_list(subfolder)
+        get_file_list(subfolder)
+
 
 allCode = {}
 
 
-
-def convert_2_target_coding(coding='utf-8'):
+def convert_2_target_coding():
     """ 转换成目标编码格式
     """
     for filepath in FileList:
+
+        # 获取文件编码
         with open(filepath, 'rb') as f:
             data = f.read()
             codeType = chardet.detect_all(data)[0]['encoding']
-        # if codeType not in (coding, 'ascii', 'UTF-8-SIG', 'Windows-1252', 'ISO-8859-1'):
-        if True:
+            # 2312范围较窄，使用18030替换
+            if codeType == "GB2312":
+                codeType = "gb18030"
+
+        # 排除已经是utf-8的代码文件
+        if codeType != "UTF-8-SIG":
+
+            # 统计功能
             print(codeType)
             if not allCode.get(codeType):
                 allCode[codeType] = 1
             else:
                 allCode[codeType] += 1
-            try:
-                with codecs.open(filepath, 'r', codeType) as f:
-                    content = f.read()
-                #写入bom
-                if CFG_SET_BOM:
-                    if CFG_SET_BOM_TYPE == "utf-8":
-                        bom_bytes = [0xEF, 0xBB, 0xBF]
-                        with codecs.open(filepath, 'wb')as f:
-                            for x in bom_bytes:
-                                a = struct.pack('B', x)
-                                f.write(a)
-                    else:
-                        print("不支持此bom类型")
-                        break
+
+            # 实际的编码转换
+            with codecs.open(filepath, 'r', codeType) as f:
+                content = f.read()
+            if CFG_SET_BOM:
+                # 写入bom签名头
+                if CFG_SET_BOM_TYPE == "utf-8":
+                    # 写入utf-8 bom签名头
+                    bom_bytes = [0xEF, 0xBB, 0xBF]
+                    with codecs.open(filepath, 'wb')as f:
+                        for x in bom_bytes:
+                            byte = struct.pack('B', x)
+                            f.write(byte)
                 else:
-                    with codecs.open(filepath, 'w')as f:
-                        f.write("")
-                # code优化提示
-                tips = "/* coding: utf-8 */\n"
-                with codecs.open(filepath, 'a', coding) as f:
-                    f.write(tips)
-                with codecs.open(filepath, 'a', coding) as f:
-                    f.write(content)
-                print(filepath + '\n')
-            except:
-                continue
+                    print("不支持此bom类型")
+                    break
+            else:
+                # 没有bom签名头需要写
+                with codecs.open(filepath, 'w')as f:
+                    f.write("")
+
+            # code优化提示，记录已经被转码
+            tips = "/* coding: utf-8 */\n"
+            with codecs.open(filepath, 'a', TargetCoding) as f:
+                f.write(tips)
+            with codecs.open(filepath, 'a', TargetCoding) as f:
+                f.write(content)
+            print(filepath + '\n')
+
 
 if __name__ == '__main__':
     # 获取目录
-    clike_files_path = "D:\\workplace\\cpp\\tencentsdk"
-    # WorkDir = str(input('input target folder\n\t:'))
+    clike_files_path = "D:\\workplace\\cpp\\RuiKeStd_Soui2.x-master"
+    # 设置工作目录
     WorkDir = clike_files_path
-    # 目标编码格式
-    TargetCoding = "utf-8"
-    # 文件类型扩展名
-    FileType = [".c", ".h", ".cpp", ".hpp", ".cc"]
     os.chdir(WorkDir)
+
+    # 获取所有需要转化编码的文件列表
     get_file_list(WorkDir)
 
-    convert_2_target_coding(TargetCoding)
+    # 对整个文件列表进行编码转化
+    convert_2_target_coding()
     print(allCode)
-
-
